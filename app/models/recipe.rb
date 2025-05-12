2dd44a5b9da862f875052154ad73b53174de0a51
Anil Patel <patelak1@gmail.com>
2025-05-12 11:30:52 +0100

    adding validations for models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,6 @@
 class Recipe < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :image_url, presence: true
+  validates :rating, presence: true
 end
